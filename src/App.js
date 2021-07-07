90448463edb0f6fb4e8c09924e309c964f4b0f6b
Zachary-Roland <zsolo95@gmail.com>
2021-07-07 14:19:49 -0500

    creates file system and adds component boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddGarm from "./components/AddGarmPage";
+import GarmDisplay from "./components/GarmDisplay";
+import GarmsContainer from "./components/GarmsContainer";
+import OutfitBuilder from "./components/OutfitBuilderPage";
+import Wardrobe from "./components/WardrobePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddGarm />
+      <GarmDisplay />
+      <GarmsContainer />
+      <OutfitBuilder />
+      <Wardrobe />
+    </>
   );
 }
 
