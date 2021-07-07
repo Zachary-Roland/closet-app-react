904cfb9ed7b6d5c533d05dfdd2e680f9aa2cdab6
Zachary-Roland <zsolo95@gmail.com>
2021-07-07 15:55:37 -0500

    adds routes to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import AddGarm from "./components/AddGarmPage";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import GarmDisplay from "./components/GarmDisplay";
 import GarmsContainer from "./components/GarmsContainer";
 import OutfitBuilder from "./components/OutfitBuilderPage";
@@ -7,13 +14,44 @@ import Wardrobe from "./components/WardrobePage";
 
 function App() {
   return (
-    <>
-      <AddGarm />
-      <GarmDisplay />
-      <GarmsContainer />
-      <OutfitBuilder />
-      <Wardrobe />
-    </>
+    <Router>
+      <nav>
+        <NavLink
+          activeClassName="active"
+          className="link text-center"
+          to="/addGarm"
+        >
+          Add Garm
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          className="link text-center"
+          to="/myGarms"
+        >
+          My Garms
+        </NavLink>
+        <NavLink
+          activeClassName="active"
+          className="link text-center"
+          to="/fitBuilder"
+        >
+          Fit Builder
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/addGarm">
+            <AddGarm />
+          </Route>
+          <Route path="/myGarms">
+            <Wardrobe />
+          </Route>
+          <Route path="/fitBuilder">
+            <OutfitBuilder />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
