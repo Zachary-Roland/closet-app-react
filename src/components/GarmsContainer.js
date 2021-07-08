f846c645ae2ec22e47d07cfa97d6ba7004641f94
Zachary-Roland <zsolo95@gmail.com>
2021-07-08 17:00:17 -0500

    adds components w/ conditionals

--- src/components/GarmsContainer.js
+++ src/components/GarmsContainer.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useContext, useMemo, useState } from "react";
+import GarmDisplay from "./GarmDisplay";
+import { GarmsContext } from "../context/GarmContext";
+
+// ! This container will filter and show either owned or wanted garments
+// TODO add state and selector to pick between owned and wanted
+// TODO 2 conditional renders GarmDisplay. One maps through owned garms, other through wanted garms.
 
 const GarmsContainer = () => {
-  return <div>GarmsContainer</div>;
+  // ! the map function will be simlilar to this function from the todo app
+  // ! but for owned key instead of ID
+  // const faveIds = useMemo(() => {
+  //   return favorites.map((val) => val.id);
+  // }, [favorites]);
+  const [closetSelect, setClosetSelect] = useState("own");
+  const { garms } = useContext(GarmsContext);
+  const ownedGarms = useMemo(() => {
+    return garms.filter((val) => val.own === "true");
+  }, [garms]);
+  const wantedGarms = useMemo(() => {
+    return garms.filter((val) => val.own === "false");
+  }, [garms]);
+  return (
+    <>
+      <div>
+        <label>Show: </label>
+        <select
+          name="closetSelect"
+          defaultValue="own"
+          value={closetSelect}
+          onChange={(e) => setClosetSelect(e.target.value)}
+        >
+          <option value="own">Owned</option>
+          <option value="want">Wanted</option>
+        </select>
+      </div>
+      {/* expected outcome w/ hardcoded garm state: 3 wanted, 5 owned */}
+      {closetSelect === "own"
+        ? ownedGarms.map(() => <GarmDisplay />)
+        : wantedGarms.map(() => <GarmDisplay />)}
+    </>
+  );
 };
 
 export default GarmsContainer;
