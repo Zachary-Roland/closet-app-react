397f04e6f9557897ec87edf379199ddad838affa
Zachary-Roland <zsolo95@gmail.com>
2021-07-09 12:27:21 -0500

    adds content to component

--- src/components/GarmDisplay.js
+++ src/components/GarmDisplay.js
@@ -1,7 +1,52 @@
 import React from "react";
 
-const GarmDisplay = () => {
-  return <div>GarmDisplay</div>;
+const GarmDisplay = ({ garm, deleteGarm, toggleOwn }) => {
+  return (
+    <div>
+      <h2>{garm.title}</h2>
+      <h4>
+        {garm.brand} - ${garm.cost} ({garm.condition})
+      </h4>
+      <h5>
+        {garm.type}, {garm.season}
+      </h5>
+      <img src={garm.img} style={{ width: 300 }}></img>
+      <div>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            deleteGarm(garm.id);
+          }}
+        >
+          Delete Garm!
+        </button>
+        {garm.own === false ? (
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              toggleOwn(garm.id, garm.own);
+              // console.log(garm.own);
+            }}
+          >
+            Mark as "Owned"
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              toggleOwn(garm.id, garm.own);
+              // console.log(garm.own);
+            }}
+          >
+            Mark as "Wanted"
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default GarmDisplay;
