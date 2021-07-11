4255830a55913fee38541cc12a2278f6a4f5ba85
Zachary-Roland <zsolo95@gmail.com>
2021-07-11 17:39:26 -0500

    adds Authguard to login comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,11 @@ import { UserContext } from "../context";
 const Login = () => {
   const [userInput, setUserInput] = useState("");
   const [password, setPassword] = useState("");
+  const [errorUser, setErrorUser] = useState(false);
+  const [errorPass, setErrorPass] = useState(false);
+  const [errorUserMsg, setErrorUserMsg] = useState("");
+  const [errorPassMsg, setErrorPassMsg] = useState("");
+  const ErrorMsg = "Must be at least 4 characters";
   const { login } = useContext(UserContext);
   const history = useHistory();
   return (
@@ -17,6 +22,8 @@ const Login = () => {
         <div className="margin50">
           <TextField
             variant="outlined"
+            error={errorUser}
+            helperText={errorUserMsg}
             label="Username:"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
@@ -24,8 +31,11 @@ const Login = () => {
         </div>
         <div className="margin50">
           <TextField
+            className="loginField"
             variant="outlined"
             type="password"
+            error={errorPass}
+            helperText={errorPassMsg}
             label="Password:"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -37,7 +47,18 @@ const Login = () => {
           style={{ marginBottom: 10 }}
           onClick={(e) => {
             e.preventDefault();
-            if (userInput.length > 4 && password.length > 4) {
+            if (userInput.length < 4 && password.length < 4) {
+              setErrorUser(true);
+              setErrorPass(true);
+              setErrorUserMsg(ErrorMsg);
+              setErrorPassMsg(ErrorMsg);
+            } else if (userInput.length < 4) {
+              setErrorUser(true);
+              setErrorUserMsg(ErrorMsg);
+            } else if (password.length < 4) {
+              setErrorPass(true);
+              setErrorPassMsg(ErrorMsg);
+            } else {
               login(userInput);
               history.push("/addGarm");
             }
