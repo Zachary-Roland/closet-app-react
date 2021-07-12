ec08448377df7dcdf80c6a345917bc5b621c9d72
Zachary-Roland <zsolo95@gmail.com>
2021-07-11 22:38:25 -0500

    adds material styling to inspiration

--- src/components/OutfitBuilderPage.js
+++ src/components/OutfitBuilderPage.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Slide } from "@material-ui/core";
+import { Slide, CircularProgress, Paper, Typography } from "@material-ui/core";
 import useFetch from "../hooks/useFetch";
 
 const OutfitBuilder = () => {
   const { data, loading, error } = useFetch();
   return (
     <>
-      {/* <Slide direction="right" in={true} mountOnEnter unmountOnExit> */}
-      <div>OutfitBuilder</div>
-      {data && <img src={data.urls.small}></img>}
-      {/* </Slide> */}
+      <Slide direction="right" in={true} mountOnEnter unmountOnExit>
+        <Paper className="margin10 compCenter">
+          <Typography variant="h3" component="h1">
+            Here's some random inspiration!
+          </Typography>
+          <Typography variant="h6" component="h1">
+            Eventually you'll be able to build an outfit here...
+          </Typography>
+          {loading && <CircularProgress />}
+          {data && <img src={data.urls.small}></img>}
+        </Paper>
+      </Slide>
     </>
   );
 };
