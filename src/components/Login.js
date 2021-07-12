abe810d63bea7cdef6464a077e3505d78c8319ba
Zachary-Roland <zsolo95@gmail.com>
2021-07-11 21:01:22 -0500

    adds mount and unmount slide to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Typography, TextField, Button, Paper } from "@material-ui/core";
+import { Typography, TextField, Button, Paper, Slide } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import { UserContext } from "../context";
 
@@ -10,63 +10,69 @@ const Login = () => {
   const [errorPass, setErrorPass] = useState(false);
   const [errorUserMsg, setErrorUserMsg] = useState("");
   const [errorPassMsg, setErrorPassMsg] = useState("");
+  const [load, setLoad] = useState(true);
   const ErrorMsg = "Must be at least 4 characters";
   const { login } = useContext(UserContext);
   const history = useHistory();
   return (
-    <Paper className="margin10 compCenter">
-      <form>
-        <Typography variant="h3" component="h3">
-          Login
-        </Typography>
-        <div className="margin50">
-          <TextField
-            variant="outlined"
-            error={errorUser}
-            helperText={errorUserMsg}
-            label="Username:"
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          />
-        </div>
-        <div className="margin50">
-          <TextField
-            variant="outlined"
-            type="password"
-            error={errorPass}
-            helperText={errorPassMsg}
-            label="Password:"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <Button
-          variant="contained"
-          color="primary"
-          style={{ marginBottom: 10 }}
-          onClick={(e) => {
-            e.preventDefault();
-            if (userInput.length < 4 && password.length < 4) {
-              setErrorUser(true);
-              setErrorPass(true);
-              setErrorUserMsg(ErrorMsg);
-              setErrorPassMsg(ErrorMsg);
-            } else if (userInput.length < 4) {
-              setErrorUser(true);
-              setErrorUserMsg(ErrorMsg);
-            } else if (password.length < 4) {
-              setErrorPass(true);
-              setErrorPassMsg(ErrorMsg);
-            } else {
-              login(userInput);
-              history.push("/addGarm");
-            }
-          }}
-        >
-          Log In!
-        </Button>
-      </form>
-    </Paper>
+    <Slide direction="right" in={load} mountOnEnter unmountOnExit>
+      <Paper className="margin10 compCenter">
+        <form>
+          <Typography variant="h3" component="h3">
+            Login
+          </Typography>
+          <div className="margin50">
+            <TextField
+              variant="outlined"
+              error={errorUser}
+              helperText={errorUserMsg}
+              label="Username:"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+          </div>
+          <div className="margin50">
+            <TextField
+              variant="outlined"
+              type="password"
+              error={errorPass}
+              helperText={errorPassMsg}
+              label="Password:"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ marginBottom: 10 }}
+            onClick={(e) => {
+              e.preventDefault();
+              if (userInput.length < 4 && password.length < 4) {
+                setErrorUser(true);
+                setErrorPass(true);
+                setErrorUserMsg(ErrorMsg);
+                setErrorPassMsg(ErrorMsg);
+              } else if (userInput.length < 4) {
+                setErrorUser(true);
+                setErrorUserMsg(ErrorMsg);
+              } else if (password.length < 4) {
+                setErrorPass(true);
+                setErrorPassMsg(ErrorMsg);
+              } else {
+                setLoad(false);
+                setTimeout(() => {
+                  login(userInput);
+                  history.push("/addGarm");
+                }, [500]);
+              }
+            }}
+          >
+            Log In!
+          </Button>
+        </form>
+      </Paper>
+    </Slide>
   );
 };
 
