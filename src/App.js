11c0ddca4c1a24f8a52b9cd74bdd49253fc4bea2
Zachary-Roland <zsolo95@gmail.com>
2021-07-11 20:29:55 -0500

    moves functions to shared folder

--- src/App.js
+++ src/App.js
@@ -11,42 +11,17 @@ import {
 // Imports for Material UI
 import "@fontsource/roboto";
 import { makeStyles } from "@material-ui/core/styles";
+import ProtectedRoute from "./shared/ProtectedRoute";
+import a11yProps from "./shared/A11yProps";
+import TabPanel from "./shared/TabPanel";
 import { AppBar, Tabs, Tab, Box, Typography } from "@material-ui/core";
 // Imports for Context
 import { UserContext } from "./context";
 // Imports for Components
-import ProtectedRoute from "./shared/ProtectedRoute";
 import Login from "./components/Login";
 import AddGarm from "./components/AddGarmPage";
 import OutfitBuilder from "./components/OutfitBuilderPage";
 import Wardrobe from "./components/WardrobePage";
-
-// Not sure what a11yProps does but it is needed for the Tabs
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography component="div">{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
 
 function App() {
   // Local value state used for Tabs Navigation
