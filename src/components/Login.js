48944e34017fd8a014a1990a56171760522408a9
Zachary-Roland <zsolo95@gmail.com>
2021-07-29 20:40:59 -0500

    adjusts uname state, login logic, adds useEffect

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,40 @@
 // Imports for React
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 // Imports for Material UI
 import { Typography, TextField, Button, Paper, Slide } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 // Imports for Context
 import { UserContext } from "../context";
+import useFetch from "../hooks/useFetch";
 
 const Login = () => {
   // states for username and password fields
-  const [userInput, setUserInput] = useState("");
+  const [username, setusername] = useState("");
   const [password, setPassword] = useState("");
   const [errorUser, setErrorUser] = useState(false);
   const [errorPass, setErrorPass] = useState(false);
   const [errorUserMsg, setErrorUserMsg] = useState("");
   const [errorPassMsg, setErrorPassMsg] = useState("");
-  const ErrorMsg = "Must be at least 4 characters";
+  const ErrorMsg = "Must be at least 4 (but no more than 20) characters";
+  const { callAPI: loginCall } = useFetch("POST");
   // state for animation
   const [load, setLoad] = useState(true);
   // importing login function
   const { login } = useContext(UserContext);
   // used to navigate on login
   const history = useHistory();
+  // A useEffect so that on component unmount, exit animation is triggered.
+  //
+  useEffect(() => {
+    return setLoad(false);
+  }, []);
   return (
-    <Slide direction="right" in={load} mountOnEnter unmountOnExit>
+    <Slide
+      direction={load ? "right" : "left"}
+      in={load}
+      mountOnEnter
+      unmountOnExit
+    >
       <Paper className="margin10 compCenter">
         <form>
           <Typography variant="h3" component="h3">
@@ -34,8 +46,8 @@ const Login = () => {
               error={errorUser}
               helperText={errorUserMsg}
               label="Username:"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
+              value={username}
+              onChange={(e) => setusername(e.target.value)}
             />
           </div>
           <div className="margin50">
@@ -53,25 +65,36 @@ const Login = () => {
             variant="contained"
             color="primary"
             style={{ marginBottom: 10 }}
-            onClick={(e) => {
+            onClick={async (e) => {
               e.preventDefault();
-              if (userInput.length < 4 && password.length < 4) {
-                setErrorUser(true);
-                setErrorPass(true);
-                setErrorUserMsg(ErrorMsg);
-                setErrorPassMsg(ErrorMsg);
-              } else if (userInput.length < 4) {
-                setErrorUser(true);
-                setErrorUserMsg(ErrorMsg);
-              } else if (password.length < 4) {
-                setErrorPass(true);
-                setErrorPassMsg(ErrorMsg);
-              } else {
-                setLoad(false);
+              setErrorUser(false);
+              setErrorPass(false);
+              if (
+                username.length > 4 &&
+                password.length > 4 &&
+                username.length <= 20 &&
+                password.length <= 20
+              ) {
+                setErrorUser(false);
+                setErrorPass(false);
+                setErrorPassMsg("");
+                setErrorUserMsg("");
+                let res = await loginCall("/api/users/login", {
+                  username,
+                  password,
+                });
                 setTimeout(() => {
-                  login(userInput);
+                  login(res.data.username, res.data.id);
                   history.push("/addGarm");
-                }, [500]);
+                }, 500);
+              }
+              if (username.length <= 4 || username.length > 20) {
+                setErrorUser(true);
+                setErrorUserMsg(ErrorMsg);
+              }
+              if (password.length <= 4 || password.length > 20) {
+                setErrorPass(true);
+                setErrorPassMsg(ErrorMsg);
               }
             }}
           >
